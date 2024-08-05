0989d19316b90988188edfdd327f76ba69a45980
RafaLNunes <rafaf.code@gmail.com>
2024-08-05 10:10:32 -0300

    s

---
+++ Biblioteca_SESI_TCC_MeanProject/Biblioteca_SESI_TCC_MeanProject/Form1.cs
@@ -0,0 +1,10 @@
+namespace Biblioteca_SESI_TCC_MeanProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
